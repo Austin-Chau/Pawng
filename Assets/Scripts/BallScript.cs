1197f0dde2b047dfc31e900c0d636dbae9ba9197
Chaukai <aznchau4ever@hotmail.com>
2018-09-04 22:14:11 -0600

    Initial commit

---
+++ Assets/Scripts/BallScript.cs
@@ -0,0 +1,90 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallScript : MonoBehaviour {
+    public float speed;
+
+
+    private SpriteRenderer sprite;
+
+    private Rigidbody2D rb;
+
+    //0 = White, 1 = Red, 2 = Blue
+    private int colorState;
+
+	// Use this for initialization
+	void Start () {
+        rb = GetComponent<Rigidbody2D>();
+        sprite = GetComponent<SpriteRenderer>();
+        ResetBall();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void changeColor(int i)
+    {
+        colorState = i;
+        switch (i)
+        {
+            case 0:
+                sprite.color = Color.white;
+                break;
+            case 1:
+                sprite.color = Color.red;
+                break;
+            case 2:
+                sprite.color = Color.blue;
+                break;
+        }
+    }
+
+    private void StartBall()
+    {
+        changeColor(0);
+        float rand = Random.Range(0, 2);
+        if (rand < 1)
+        {
+            rb.AddForce(new Vector2(20, Random.Range(-15,15)) * speed);
+        }
+        else
+        {
+            rb.AddForce(new Vector2(-20, Random.Range(-15,15)) * speed);
+        }
+    }
+
+    private void ResetBall()
+    {
+        rb.velocity = Vector2.zero;
+        rb.transform.position = Vector2.zero;
+        Invoke("StartBall", 2);
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+
+        
+
+        if (collision.collider.name == "GoalRight")
+        {
+            ResetBall();
+        } 
+        if (collision.collider.name == "GoalLeft")
+        {
+            ResetBall();
+        }
+        if (collision.collider.tag == "Player")
+        { 
+            rb.AddForce(collision.collider.GetComponent<Rigidbody2D>().velocity * speed);
+            if (sprite.color == Color.white) changeColor(collision.collider.GetComponent<PlayerScript>().colorState);
+            else
+            {
+                if (collision.collider.GetComponent<PlayerScript>().colorState != colorState) ResetBall();
+                else changeColor(0);
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/PlayerScript.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerScript : MonoBehaviour {
+    public KeyCode Up;
+    public KeyCode Down;
+    public KeyCode Red;
+    public KeyCode Blue;
+    public float speed;
+
+    private SpriteRenderer sprite;
+    private Rigidbody2D rb;
+
+    //0 = White, 1 = Red, 2 = Blue
+    public int colorState;
+
+	// Use this for initialization
+	void Start () {
+        rb = GetComponent<Rigidbody2D>();
+        sprite = GetComponent<SpriteRenderer>();
+        colorState = 0;
+    }
+
+    private void changeColor(int i)
+    {
+        colorState = i;
+        switch (i)
+        {
+            case 0:
+                sprite.color = Color.white;
+                break;
+            case 1:
+                sprite.color = Color.red;
+                break;
+            case 2:
+                sprite.color = Color.blue;
+                break;
+        }
+    }
+
+    // Update is called once per frame
+    void Update () {
+        bool isOut = false;
+        int dir = 0;
+        if (Input.GetKey(Up)) dir = 1;
+        else if (Input.GetKey(Down)) dir = -1;
+
+        if (rb.position.y <= -4 && dir < 0) isOut = true;
+        else if (rb.position.y >= 4 && dir > 0) isOut = true;
+
+        if (!isOut)
+        {
+            rb.transform.position += new Vector3(0, dir * speed);
+        }
+
+        if (Input.GetKey(Red)) changeColor(1);
+        else if (Input.GetKey(Blue)) changeColor(2);
+	}
+
+
+}
